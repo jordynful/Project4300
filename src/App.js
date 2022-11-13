2b717fcd7787678e01608905c1b6b670ec7636b7
Jordyn Fulbright <jordynfulbright@gmail.com>
2022-11-13 13:28:03 -0500

    do not merge this, it is messwed up but we will work more on it during the login auth with mongodb part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { useState, useCallback } from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch
+  Navigate
 } from 'react-router-dom';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { UserProvider } from "./contexts/user.context";
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import Menu from './places/pages/Menu';
@@ -13,6 +11,7 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
+// require database connection 
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -25,52 +24,82 @@ const App = () => {
     setIsLoggedIn(false);
   }, []);
 
-  let routes;
+  let routes = (
+    <Routes>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/places" exact>
+        <Menu />
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlace />
+      </Route>
+      <Route path="/auth">
+        <Auth />
+      </Route>
+      <Navigate to="/" />
+    </Routes>
+  );
 
-  if (isLoggedIn) {
-    routes = (
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places" exact>
-          <Menu />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Route path="/places/:placeId">
-          <UpdatePlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    );
-  } else {
-    routes = (
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places" exact>
-          <Menu />
-        </Route>
-        <Route path="/auth">
-          <Auth />
-        </Route>
-        <Redirect to="/auth" />
-      </Switch>
-    );
-  }
+  // if (isLoggedIn) {
+  //   routes = (
+  //     <Routes>
+  //       <Route path="/" exact>
+  //         <Users />
+  //       </Route>
+  //       <Route path="/places" exact>
+  //         <Menu />
+  //       </Route>
+  //       <Route path="/places/new" exact>
+  //         <NewPlace />
+  //       </Route>
+  //       <Route path="/places/:placeId">
+  //         <UpdatePlace />
+  //       </Route>
+  //       <Navigate to="/" />
+  //     </Routes>
+  //   );
+  // } else {
+  //   routes = (
+  //     <Routes>
+  //       <Route path="/" exact>
+  //         <Users />
+  //       </Route>
+  //       <Route path="/places" exact>
+  //         <Menu />
+  //       </Route>
+  //       <Route path="/auth">
+  //         <Auth />
+  //       </Route>
+  //       <Navigate to="/auth" />
+  //     </Routes>
+  //   );
+  // }
 
   return (
-    <AuthContext.Provider
-      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
-    >
-      <Router>
-      <MainNavigation />
-        <main>{routes}</main>
-      </Router>
-    </AuthContext.Provider>
+    
+
+<BrowserRouter>
+<MainNavigation>
+{/* We are wrapping our whole app with UserProvider so that */}
+{/* our user is accessible through out the app from any page*/}
+<UserProvider>
+  <Routes>
+    <main>{routes}</main>
+    {/* <Route exact path="/" element={<Users />} />
+    <Route path="/auth" element={<Auth />} />
+    <Route path="/places" >
+    <Menu />
+    </Route> */}
+    {/* We are protecting our Home Page from unauthenticated */}
+    {/* users by wrapping it with PrivateRoute here. */}
+  </Routes>
+</UserProvider>
+</MainNavigation>
+</BrowserRouter>
+
+
   );
 };
 
